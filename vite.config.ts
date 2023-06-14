502a2fdb159ebc84067c5a2e2d24f63687452301
Anna Egorova <a.egorova@brightlab.me>
2023-06-14 12:49:29 +0300

    update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export const configObj: UserConfigExport = {
     https: true,
     port: 3000
   },
-  base: '/sunnyydv.github.io',
+  base: '/sunnyydv.github.io/connox/',
   publicDir: './static',
   build: {
     outDir: './public',
